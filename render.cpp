9e40c6083adaf3178006270195701452abeb1741
Ryan Murulo <rkruberg@gmail.com>
2025-07-16 21:38:02 +0300

    refactor: rendering system
    A class for basic texture rendering
    standardised render calls
    load render happens after offsset and player have been updated

--- render.cpp
+++ render.cpp
@@ -10,19 +10,35 @@
 #include <algorithm>
 #include <unordered_set>
 
+RenderQueueItem::RenderQueueItem(int z_index, SDL_Rect dstrect, Texture* texture) {
+    this->z_index = z_index;
+    this->dstrect = dstrect;
+    this->texture = texture;
+}
+RenderQueueItem::RenderQueueItem(int z_index, std::function<void(SDL_Renderer* renderer)> custom_render) {
+    this->z_index = z_index;
+    this->custom_render = custom_render;
+}
+
+void RenderQueueItem::render(SDL_Renderer* renderer) {
+    if (this->texture.has_value()) {
+        return this->texture.value()->render(renderer, &this->dstrect);
+    }
+    if (this->custom_render.has_value()) {
+        return this->custom_render.value()(renderer);
+    }
+
+    throw std::logic_error("texture or render function needed for rendering");
+}
+
 std::unordered_map<std::pair<int, int>, int, pair_hash> random_offsets;
 static const std::unordered_set<int> ground_values = { 1, 2, 8 };
 
 void render_map(SDL_Renderer* renderer, struct Offset& offset, struct Player& player) {
-
-    struct Renderable {
-        SDL_Texture* tex = nullptr;
-        SDL_Rect dest;
-        int sortY;
-        std::function<void()> custom_draw = nullptr; // Optional for special draw logic
-    };
-
-    std::vector<Renderable> render_queue;  // render q's ei ole groundi pildid ehk id 1, 4, 5. V6ib lisanduda!
+    // render q's ei ole groundi pildid ehk id 1, 4, 5. V6ib lisanduda!
+    std::vector<RenderQueueItem> render_queue;
+    int render_diameter = render_radius * 2;
+    render_queue.reserve(render_diameter * render_diameter);
 
     int player_tile_x = static_cast<int>(player.x / tile_size);
     int player_tile_y = static_cast<int>(player.y / tile_size);
@@ -40,80 +56,77 @@ void render_map(SDL_Renderer* renderer, struct Offset& offset, struct Player& pl
         for (int column = 0; column < map_size; column++) {
             if (column < left || column > right) continue;
 
-
-            SDL_FPoint isometric_coordinates = to_isometric_grid_coordinate(offset, column, row);
-
-            int row_coord = isometric_coordinates.x;
-            int col_coord = isometric_coordinates.y;
-
-            SDL_Rect destTile = { row_coord, col_coord, tile_size, tile_size };
-
             int grid_value = map[row][column];
             std::pair<int, int> grid_pos = { row, column };
 
+            SDL_FPoint isometric_coordinates = to_isometric_grid_coordinate(offset, column, row);
+            int row_coord = isometric_coordinates.x;
+            int col_coord = isometric_coordinates.y;
+            SDL_Rect destTile = { row_coord, col_coord, tile_size, tile_size };
+
             // Searching for grid_value in ground_values
-            auto it = ground_values.find(grid_value);
             if (ground_values.find(grid_value) != ground_values.end()) {
-                load_cube_ground_texture(renderer, destTile);
+                texture_map[Map::GROUND_CUBE].render(renderer, &destTile);
             }
 
 
             switch (grid_value)
             {
-            case 2: { // tree.png
-                destTile.y -= half_tile;
-                render_queue.push_back(Renderable{ tree_tex, destTile, destTile.y });
-                break;
-            }
-            case 4: { // maze_ground_cube.png
-                load_cube_maze_ground_texture(renderer, destTile);
-                break;
-            }
-            case 5: { // snowy_ground_cube.png
-                load_cube_snowy_ground_texture(renderer, destTile);
-                break;
-            }
-            case 6: { // yellow_cube.png
-                load_cube_yellow_texture(renderer, destTile);
-                break;
-            }
-            case 7: { // error_cube.png
-                load_cube_error_texture(renderer, destTile);
-                break;
-            }
-            case 66: { // blue_cube.png
-                load_cube_blue_texture(renderer, destTile);
+                // simple textures that can be immediately rendered
+            case Map::MAZE_GROUND_VAL:
+            case Map::SNOWY_GROUND_VAL:
+            case Map::YELLOW_VAL:
+            case Map::ERROR_VAL:
+            case Map::BLUE_VAL: {
+                texture_map[grid_value].render(renderer, &destTile);
                 break;
             }
 
-            case 9: case 91: case 93: { // walls | wall_cube.png
+            case Map::TREE_VAL: {
                 destTile.y -= half_tile;
-                render_queue.push_back(Renderable{ cube_wall_tex, destTile, destTile.y });
+                render_queue.push_back(
+                    RenderQueueItem(destTile.y, destTile, &texture_map[grid_value])
+                );
                 break;
             }
-            case 94: { // hairy bottom walls | ingrown_wall_cube.png
+                              // these tree use the same texture atm
+            case Map::WALL_VAL:
+            case Map::SECTOR_1_WALL_VAL:
+            case Map::SECTOR_3_WALL_VAL: {
                 destTile.y -= half_tile;
-                render_queue.push_back(Renderable{ cube_ingrown_wall_tex, destTile, destTile.y });
+                render_queue.push_back(
+                    RenderQueueItem(destTile.y, destTile, &texture_map[Map::WALL_VAL])
+                );
                 break;
             }
-            case 8: case 92: { // wall cubes with vines
-                load_cube_ground_texture(renderer, destTile);
+            case Map::INGROWN_WALL_VAL: {
+                destTile.y -= half_tile;
+                render_queue.push_back(
+                    RenderQueueItem(destTile.y, destTile, &texture_map[Map::INGROWN_WALL_VAL])
+                );
+                break;
+            }
+            case Map::WALL_VAL_VINE: case Map::SECTOR_2_WALL_VAL: {
+                texture_map[Map::GROUND_CUBE].render(renderer, &destTile);
                 // Only generate random offset once per block
-                if (random_offsets.find(grid_pos) == random_offsets.end()) {
-                    random_offsets[grid_pos] = rand() % 20;  // see 20 on kui palju px on maxist v2hem
-                }
-                int xr = random_offsets[grid_pos];
+                // see 20 on kui palju px on maxist v2hem
+                int xr = random_offsets.try_emplace(grid_pos, rand() % 20)
+                    .first->second;
 
                 destTile.y -= half_tile - (xr / 2);
                 destTile.x += (xr / 2);
                 destTile.w -= xr;
                 destTile.h -= xr;
 
-                render_queue.push_back(Renderable{ cube_wall_tex, destTile, destTile.y });
+                render_queue.push_back(
+                    RenderQueueItem(destTile.y, destTile, &texture_map[Map::WALL_VAL])
+                );
 
                 destTile.y += 1;  // +1 sest muidu hakkab walli destTile'iga v6itlema ja flickerib.
                 auto cube_vine_tex = choose_cube_vine_texture("", grid_pos);
-                render_queue.push_back(Renderable{ cube_vine_tex, destTile, destTile.y });
+                render_queue.push_back(
+                    RenderQueueItem(destTile.y, destTile, cube_vine_tex)
+                );
                 break;
             }
 
@@ -139,22 +152,19 @@ void render_map(SDL_Renderer* renderer, struct Offset& offset, struct Player& pl
                                  static_cast<int>(player.rect.w),
                                  static_cast<int>(player.rect.h)
     };
-    render_queue.push_back(Renderable{ nullptr, player_int_rect, player_int_rect.y, [=]() { draw_player(renderer, offset); } });
+    render_queue.push_back(
+        RenderQueueItem(player_int_rect.y, [&offset](SDL_Renderer* renderer) { draw_player(renderer, offset); })
+    );
 
-    // Sort
-    std::sort(render_queue.begin(), render_queue.end(), [](const Renderable& a, const Renderable& b) {
-        return a.sortY < b.sortY;
+    std::sort(render_queue.begin(), render_queue.end(), [](const RenderQueueItem& a, const RenderQueueItem& b) {
+        return a.z_index < b.z_index;
         });
 
-    // Render all
+    // render things in queue
     for (auto& r : render_queue) {
-        if (r.custom_draw) {
-            r.custom_draw();
-        }
-        else if (r.tex) {
-            SDL_RenderCopy(renderer, r.tex, nullptr, &r.dest);
-        }
+        r.render(renderer);
     }
+    render_queue.clear();
 }
 
 
@@ -177,10 +187,8 @@ void render_map_numbers(SDL_Renderer* renderer, struct Offset& offset, struct Pl
             if (x < left || x > right) continue;
 
             SDL_FPoint isometric_coordinates = to_isometric_grid_coordinate(offset, x, y);
-
             int row_coord = isometric_coordinates.x;
             int col_coord = isometric_coordinates.y;
-
             SDL_Rect destTile = { row_coord + (tile_size / 4), col_coord, half_tile, half_tile };
 
             load_specific_number(renderer, map[y][x], destTile);
